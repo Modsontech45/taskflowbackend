8cce55d48d17ef8904f49155a69a117db3619dee
Modson Tande <131668620+Modsontech45@users.noreply.github.com>
2025-08-31 03:24:48 +0000

    hello

--- src/app.js
+++ src/app.js
@@ -27,9 +27,7 @@ const allowedOrigins = [
   "https://bolt.new",
   "http://localhost:3000",
   "http://192.168.1.142:3000",
-  "https://rfid-attendance-synctuario-theta.vercel.app",
   "https://taskflow-nine-henna.vercel.app",
-  "https://rfid-attendancesystem-backend-project.onrender.com",
   "https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--96435430.local-credentialless.webcontainer-api.io",
 ];
 
